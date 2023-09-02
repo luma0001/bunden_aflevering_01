12762255c32ee1f3416bf4d09dc5f71c136c351f
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-09-02 12:52:15 +0200

    5th commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import corse from "cors";
 // import artists from "./artists.json";
 
@@ -12,44 +12,74 @@ const cors = use(cors());
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`serveren køre på http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`serveren køre på http://localhost:${port}`);
+// });
 
-app.get("/artists", (request, response) => {
-  request.send("SHOW ARITISTS");
-});
+// app.get("/artists", (request, response) => {
+//   request.send("SHOW ARITISTS");
+// });
 
-app.post("/artists", (request, response) => {
-  const artist = "INSERT_OBJECT";
-  artists.push(artist);
-  request.send("POST ARTIST");
-});
+// app.post("/artists", (request, response) => {
+//   const artist = "INSERT_OBJECT";
+//   artists.push(artist);
+//   request.send("POST ARTIST");
+// });
 
-app.put("/artists/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const artist = artists.find((singer) => singer.id === id);
-  const body = response.body;
+// app.put("/artists/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   const artist = artists.find((singer) => singer.id === id);
+//   const body = response.body;
 
-  artist.name = body.name;
-  artist.birthdate = body.birthdate;
-  artist.activeSince = body.activeSince;
-  artist.genres = body.genres;
-  artist.labels = body.labels;
-  artist.website = body.website;
-  artist.image = body.image;
-  artist.shortDescription = body.shortDescription;
+//   artist.name = body.name;
+//   artist.birthdate = body.birthdate;
+//   artist.activeSince = body.activeSince;
+//   artist.genres = body.genres;
+//   artist.labels = body.labels;
+//   artist.website = body.website;
+//   artist.image = body.image;
+//   artist.shortDescription = body.shortDescription;
 
-  request.send("PUT ARTIST");
-});
+//   request.send("PUT ARTIST");
+// });
 
-app.patch("/artists", (request, response) => {
-  request.send("PATCH ARTIST");
-});
+// app.patch("/artists", (request, response) => {
+//   request.send("PATCH ARTIST");
+// });
 
-app.delete("/artists/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const artist = artists.find((singer) => singer.id === id);
-  request.send("DELETE ARTIST");
-  // request.delte(artist);
+// app.delete("/artists/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   const artist = artists.find((singer) => singer.id === id);
+//   request.send("DELETE ARTIST");
+//   // request.delte(artist);
+// });
+
+app.listen(port, (request, response) => {
+  if (request.url === "/artists" && request.method === "GET") {
+    console.log(`serveren køre på http://localhost:${port}`);
+    request.send("SHOW ARITISTS");
+  } else if (request.url === "/artists" && request.method === "POST") {
+    const artist = "INSERT_OBJECT";
+    artists.push(artist);
+    request.send("POST ARTIST");
+  } else if (request.url === "/artists" && request.method === "PUT") {
+    const id = Number(request.params.id);
+    const artist = artists.find((singer) => singer.id === id);
+    const body = response.body;
+
+    artist.name = body.name;
+    artist.birthdate = body.birthdate;
+    artist.activeSince = body.activeSince;
+    artist.genres = body.genres;
+    artist.labels = body.labels;
+    artist.website = body.website;
+    artist.image = body.image;
+    artist.shortDescription = body.shortDescription;
+
+    request.send("PUT ARTIST");
+  } else if (request.url === "/artists" && request.method === "delete") {
+    const id = Number(request.params.id);
+    const artist = artists.find((singer) => singer.id === id);
+    request.send("DELETE ARTIST");
+  }
 });
